f18fa39cea0ae57653bfee859249ff5e35fdd379
abhishekpundir789 <apundir@algomau.ca>
2021-04-13 10:03:46 -0700

    update commit

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import './Components/Header'
 
 function App() {
   return (
     <div className="App">
       <h1> Hello World!</h1>
-      {/* Header */}
-      <Header />
+       {/* <Header /> */}
       {/* Home */}
     </div>
   );
