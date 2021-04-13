08188ad4757bfd698b347354a74432df2a486ffc
abhishekpundir789 <apundir@algomau.ca>
2021-04-13 16:03:56 -0700

    update commit with navigation bar setup

--- e-commerce/src/Header.js
+++ e-commerce/src/Header.js
@@ -4,13 +4,26 @@ import './Header.css'
 function Header() {
     return (
         <div className='header'>
-            <img className = "header__logo" src="Images/Capture.JPG" />
+            <img className = "header__logo" src="Capture.JPG" alt ="Store logo"/>
+            <h2>E-Commerce Store</h2>
             
             <div className="header__search"> 
                 <input className="header__searchInput" type="text" />
             </div>
 
             <div className ="header__nav">
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
+                </div>
+                <div className='header__option'>
+                <span className='header__optionLineOne'>Returns</span>
+                <span className='header__optionLineTwo'> & Orders</span>
+                </div>
+                <div className='header__option'>
+                <span className='header__optionLineOne'>Your</span>
+                <span className='header__optionLineTwo'>Prime</span>
+                </div>
                 
 
             </div>
