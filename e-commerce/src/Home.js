d9d0f1a7aca54729b39bdd732756ddb33f8960b8
abhishekpundir789 <apundir@algomau.ca>
2021-04-14 14:01:31 -0700

    update commit with the product layout page

--- e-commerce/src/Home.js
+++ e-commerce/src/Home.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
         <div className ="home">
             <div className="home__container">
-                <img src="https://i.gadgets360cdn.com/large/best_movies_on_amazon_prime_august_2020_1597408336879.jpg" alt="prime video source" />
-            </div>
+                <img className= "home__image" src="https://i.gadgets360cdn.com/large/best_movies_on_amazon_prime_august_2020_1597408336879.jpg" alt="prime video source" />
             
+            <div className="home__row">
+                <Product />
+                <Product />
+            </div>
+
+            <div className="home__row">
+                { /*Product*/ }
+                { /*Product*/ }
+                { /*Product*/ }
+            </div>
+
+            <div className="home__row">
+                { /*Product*/ }
+            </div>
+
+            </div>  
         </div>
     )
 }
---
+++ e-commerce/src/Product.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Product() {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>The Lean Startup</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>25.68</strong>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Product
