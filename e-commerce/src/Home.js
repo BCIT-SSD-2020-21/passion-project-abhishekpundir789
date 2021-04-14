75e625daf09e9e7e7e44ecd37c997812cead76e1
abhishekpundir789 <apundir@algomau.ca>
2021-04-14 10:30:43 -0700

    update commit with the banner image and home component

---
+++ e-commerce/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+    return (
+        <div className ="home">
+            <div className="home__container">
+                <img src="https://i.gadgets360cdn.com/large/best_movies_on_amazon_prime_august_2020_1597408336879.jpg" alt="prime video source" />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
