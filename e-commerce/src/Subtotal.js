3c122e39e87ed3d03787b325acaccf9d7a9b6748
abhishekpundir789 <apundir@algomau.ca>
2021-04-15 10:41:11 -0700

    update commit with price calculate in cart

--- e-commerce/src/Subtotal.js
+++ e-commerce/src/Subtotal.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="subtotal">
             <CurrencyFormat 
                 renderText={(value) => (
                   <>
-                    <p>Subtotal (0 items): <strong>0</strong></p>
+                    <p>Subtotal ({basket?.length} items): <strong>{value}</strong></p>
                     <small className="subtotal__gift"><input type="checkbox" />This order contains gift card</small>
                   </>  
                 )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={"text"}
                 thousanSeparator={true}
                 prefix={"$"}
