8c97c1f8e4a632580dba337cb9fd0381ab58459a
abhishekpundir789 <apundir@algomau.ca>
2021-04-16 04:47:11 -0700

    update commit with the userId in database and routing

--- e-commerce/src/Payment.js
+++ e-commerce/src/Payment.js
@@ -7,6 +7,7 @@ import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
 import CurrencyFormat from 'react-currency-format';
 import { getBasketTotal } from './reducer';
 import axios from './Axios';
+import { db } from './Firebase/firebase'
 
 function Payment() {
     const [{basket, user}, dispatch] = useStateValue();
@@ -43,6 +44,17 @@ function Payment() {
                 card: elements.getElement(CardElement)
             }
         }).then(({ paymentIntent }) => {
+
+            db.collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .doc(paymentIntent.id)
+            .set({
+                basket: basket,
+                amount: paymentIntent.amount,
+                created: paymentIntent.created
+            })
+
             setSucceeded(true);
             setError(null);
             setProcessing(false)
