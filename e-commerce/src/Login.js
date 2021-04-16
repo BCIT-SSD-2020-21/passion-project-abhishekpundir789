16ffd4bdfd94501710fe86f11b5dc0241ff6029c
abhishekpundir789 <apundir@algomau.ca>
2021-04-15 23:52:24 -0700

    update commit with the signIn functionality

--- e-commerce/src/Login.js
+++ e-commerce/src/Login.js
@@ -6,16 +6,22 @@ import { auth } from './Firebase/firebase';
 function Login() {
     const history = useHistory();
     const [email, setEmail] = useState('');
-    const [passowrd, setPassword] = useState('');
+    const [password, setPassword] = useState('');
 
     const signIn = e => {
         e.preventDefault();
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
     }
 
     const register = e => {
         e.preventDefault();
         auth
-            .createUserWithEmailAndPassword(email, passowrd)
+            .createUserWithEmailAndPassword(email, password)
             .then((auth) => {
                 console.log(auth);
                 if(auth){
@@ -38,7 +44,7 @@ function Login() {
                     <h4>Email</h4>
                     <input type="text" value={email} onChange={ e => setEmail(e.target.value)} />
                     <h4>Password</h4>
-                    <input type="password" value={passowrd} onChange={e => setPassword(e.target.value)} />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
                     <button  type = 'submit' onClick = {signIn} className="login__signInButton" > Sign In </button><br></br>
                     <button onClick = {register} className="login__registerButton">Create New Account </button>
