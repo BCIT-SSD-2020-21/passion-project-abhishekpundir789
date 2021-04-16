fd8a06d80543f9d7ac756d9c36be21512dbecaf2
abhishekpundir789 <apundir@algomau.ca>
2021-04-16 01:32:46 -0700

    update commit with styling and rendering product in payment page

--- e-commerce/src/Payment.js
+++ e-commerce/src/Payment.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import CheckoutProduct from './CheckoutProduct';
 import './Payment.css';
+import { useStateValue } from './StateProvider';
 
 function Payment() {
+    const [{basket, user}, dispatch] = useStateValue();
+
     return (
         <div className='payment'>
             <div className='payment__container'>
@@ -9,10 +13,26 @@ function Payment() {
                     <div className='payment__title'>
                         <h3>Delivery Address</h3>
                     </div>
+                    <div className='payment__address'>
+                        <p>{user?.email}</p>
+                        <p>123 Surrey</p>
+                        <p>BC, CA</p>
+                    </div>
                 </div>
 
                 <div className="payment__section">
-                    
+                    <div className='payment__title'>
+                        <h3> Review Item and Delivery </h3>
+                    </div>
+                    <div className="payment__items">
+                        {basket.map(item =>(
+                            <CheckoutProduct 
+                                title={item.title}
+                                price={item.price}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
                 </div>
 
                 <div className="payment__section">
